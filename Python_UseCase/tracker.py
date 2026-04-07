81c3753a688a49f2e396bf5a7ebdaa0bc504fd1a
Yeshwanth Ashala <your_email@example.com>
2026-04-07 12:46:21 +0530

    Built Tkinter GUI and linked to CSV storage

--- Python_UseCase/tracker.py
+++ Python_UseCase/tracker.py
@@ -2,11 +2,23 @@ import os
 import csv
 
 
-def add_expense(data,category,amount,description):
+def add_expense(date,category,amount,description):
     file_name='expenses.csv'
+    try:
+        amount=float(amount)
+        if amount<=0 or not date or not category or not description:
+            raise Exception("Invalid Input")
+    except Exception:
+        return "Enter valid data"
+    if not os.path.exists(file_name):
+        with open(file_name, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['Date', 'Category', 'Amount', 'Description'])
+    with open(file_name, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow([date, category, amount, description])
 
-
-    return "SuccessFully Added"
+    return "Successfully Added"
 
 
 
